eb940001aea7b3de9104fe30735d83d5be535860
github username <github email address>
2017-09-01 08:10:38 -0400

    Removed excess code

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -21,7 +21,6 @@ export default class SearchBar extends Component {
   fetchResults(response) {
     for (var i = 0; i < response.items.length; i++) {
       var item = response.items[i];
-      // in production code, item.htmlTitle should have the HTML entities escaped.
       document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
     }
   }
