476bce3287008ec0046a06bf9dd13982dd2b2b32
github username <github email address>
2017-09-03 15:02:16 -0400

    Google search

--- src/google_searcher.js
+++ src/google_searcher.js
@@ -1,82 +1,105 @@
-export const googleSearcher = (n) => {
+import { request } from 'request';
+import { cheerio } from 'cheerio';
+import { querystring } from 'querystring';
+import { util } from 'util';
+// var request = require('request')
+// var cheerio = require('cheerio')
+// var querystring = require('querystring')
+// var util = require('util')
 
-  var request = require("request"),
-  	cheerio = require("cheerio"),
-  	url = "https://www.google.com/search?q=data+mining",
+export const googleSearcher = () => {
+  var linkSel = 'h3.r a'
+  var descSel = 'div.s'
+  var itemSel = 'div.g'
+  var nextSel = 'td.b a span'
+//
+  var URL = '%s://www.google.%s/search?hl=%s&q=%s&start=%s&sa=N&num=%s&ie=UTF-8&oe=UTF-8&gws_rd=ssl'
+//
+  var nextTextErrorMsg = 'Translate `google.nextText` option to selected language to detect next results link.'
+  var protocolErrorMsg = "Protocol `google.protocol` needs to be set to either 'http' or 'https', please use a valid protocol. Setting the protocol to 'https'."
 
-  	list = {},
-  	totalResults = 0,
-  	resultsDownloaded = 0;
-
-  function callback() {
-  	resultsDownloaded++;
-
-  	if (resultsDownloaded !== totalResults) {
-  		return;
-  	}
-
-  	var words = [];
-
-  	for (n in list) {
-  		words.push({
-  			word: n,
-  			count: list[n]
-  		});
-  	}
-
-  	words.sort(function (a, b) {
-  		return b.count - a.count;
-  	});
-
-  	console.log(words.slice(0, 20));
+  function google (query, start, callback) {
+    var startIndex = 0
+    if (typeof callback === 'undefined') {
+      callback = start
+    } else {
+      startIndex = start
+    }
+    igoogle(query, startIndex, callback)
   }
 
-  request(url, function (error, response, body) {
-  	if (error) {
-  		console.log(error);
-  		return;
-  	}
+  google.resultsPerPage = 10
+  google.tld = 'com'
+  google.lang = 'en'
+  google.requestOptions = {}
+  google.nextText = 'Next'
+  google.protocol = 'https'
 
-  	var $ = cheerio.load(body),
-  		links = $(".r a");
+  var igoogle = function (query, start, callback) {
+    if (google.resultsPerPage > 100) google.resultsPerPage = 100
+    if (google.lang !== 'en' && google.nextText === 'Next') console.warn(nextTextErrorMsg)
+    if (google.protocol !== 'http' && google.protocol !== 'https') {
+      google.protocol = 'https'
+      console.warn(protocolErrorMsg)
+    }
 
-  	links.each(function(i, link) {
-  		var url = $(link).attr("href");
-  		url = url.replace("/url?q=", "").split("&")[0];
+    if (google.timeSpan) {
+      URL = URL.indexOf('tbs=qdr:') >= 0 ? URL.replace(/tbs=qdr:[snhdwmy]\d*/, 'tbs=qdr:' + google.timeSpan) : URL.concat('&tbs=qdr:', google.timeSpan)
+    }
 
-  		if (url.charAt(0) === "/") {
-  			return;
-  		}
+    var newUrl = util.format(URL, google.protocol, google.tld, google.lang, querystring.escape(query), start, google.resultsPerPage)
+    var requestOptions = {
+      url: newUrl,
+      method: 'GET'
+    }
 
-  		totalResults++;
+    for (var k in google.requestOptions) {
+      requestOptions[k] = google.requestOptions[k]
+    }
 
-  		request(url, function(error, response, body) {
-  			if (error) {
-  				console.log(error);
-  				return;
-  			}
+    request(requestOptions, function (err, resp, body) {
+      if ((err == null) && resp.statusCode === 200) {
+        var $ = cheerio.load(body)
+        var res = {
+          url: newUrl,
+          query: query,
+          start: start,
+          links: [],
+          $: $,
+          body: body
+        }
 
-  			var $page = cheerio.load(body),
-  				text = $page("body").text();
+        $(itemSel).each(function (i, elem) {
+          var linkElem = $(elem).find(linkSel)
+          var descElem = $(elem).find(descSel)
+          var item = {
+            title: $(linkElem).first().text(),
+            link: null,
+            description: null,
+            href: null
+          }
+          var qsObj = querystring.parse($(linkElem).attr('href'))
 
-  			text = text.replace(/\s+/g, " ")
-  					   .replace(/[^a-zA-Z ]/g, "")
-  					   .toLowerCase();
+          if (qsObj['/url?q']) {
+            item.link = qsObj['/url?q']
+            item.href = item.link
+          }
 
-  			text.split(" ").forEach(function (word) {
-  				if (word.length < 4 || word.length > 20) {
-  					return;
-  				}
+          $(descElem).find('div').remove()
+          item.description = $(descElem).text()
 
-  				if (list[word]) {
-  					list[word]++;
-  				} else {
-  					list[word] = 1;
-  				}
-  			});
+          res.links.push(item)
+        })
 
-  			callback();
-  		});
-  	});
-  });
+        if ($(nextSel).last().text() === google.nextText) {
+          res.next = function () {
+            igoogle(query, start + google.resultsPerPage, callback)
+          }
+        }
+        callback(null, res)
+      } else {
+        callback(new Error('Error on response' + (resp ? ' (' + resp.statusCode + ')' : '') + ':' + err + ' : ' + body), null, null)
+      }
+    })
+  }
 }
